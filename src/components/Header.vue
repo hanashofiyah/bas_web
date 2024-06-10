4677b9ac3274ea33a494bacb138fa58c6d849845
unknown <hana.shofiyah23@gmail.com>
2024-06-11 00:20:41 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <h1>
+            {{ text }}
+        </h1>
+        <slot />
+        <slot name="part1_header" :param1="data.param_from_slot"></slot>
+    </div>
+</template>
+<script setup>
+
+import { reactive } from 'vue';
+
+const data = reactive({
+    param_from_slot: "Hellow form part1 header"
+})
+
+defineProps({
+    text: {
+        type: String,
+        default: " "
+    }
+})
+</script>
